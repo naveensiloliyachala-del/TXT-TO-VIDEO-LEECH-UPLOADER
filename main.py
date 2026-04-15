7a7e376b2c5e860a5aca1c62f855db065f4e60da
naveensiloliyachala-del <naveensiloliyachala@gmail.com>
2026-04-15 09:32:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def force_subscribe(func):
             is_sub = await is_subscribed(bot, message.from_user.id)
             if not is_sub:
                 keyboard = InlineKeyboardMarkup([
-                    [InlineKeyboardButton("🔔 Join Channel", url="https://t.me/roxybasicneedbot1")],
+                    [InlineKeyboardButton("🔔 Join Channel", url="https://t.me/shem_2_u")],
                     [InlineKeyboardButton("🔄 Refresh", callback_data="refresh_sub")]
                 ])
                 await message.reply_text(
